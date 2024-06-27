ffeee59b20a74d94e64c241a875473cd2cb1d6db
Nacir <chahinnacir@gmail.com>
2024-06-27 17:28:40 +0300

    redirect links fix

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/';
+    protected string $redirectTo = '/';
 
     /**
      * Create a new controller instance.
